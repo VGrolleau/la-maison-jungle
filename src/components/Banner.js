9a42bee15311da06b0b104a03ea32f2cc5ca922d
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-10-29 17:35:01 +0200

    View Lists and conditions in React

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,15 @@ import logo from '../assets/logo.png';
 
 function Banner() {
     const title = "La maison jungle";
-    return (<div className="lmj-banner">
-        <img src={logo} alt={title} className='lmj-logo' />
-        <h1 className='lmj-title'>{title}</h1>
-    </div>);
+
+    return (
+        <div className="lmj-banner">
+            <div id='lmj-banner-row'>
+                <img src={logo} alt={title} className='lmj-logo' />
+                <h1 className='lmj-title'>{title}</h1>
+            </div>
+        </div>
+    );
 }
 
 export default Banner;

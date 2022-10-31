eb8bd03fbe18e10302887dc3b978c7f63db9ee9d
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-10-31 14:53:22 +0100

    View useEffect in React

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
@@ -7,6 +7,10 @@ function Cart({ cart, updateCart }) {
         (acc, plantType) => acc + plantType.amount * plantType.price,
         0
     )
+    useEffect(() => {
+        document.title = `LMJ: ${total}€ d'achats`
+    }, [total])
+
     return isOpen ? (
         <div className='lmj-cart'>
             <button
@@ -15,15 +19,22 @@ function Cart({ cart, updateCart }) {
             >
                 Fermer
             </button>
-            <h2>Panier</h2>
-            {cart.map(({ name, price, amount }, index) => (
-                <div key={`${name}-${index}`}>
-                    {name} {price}€ x {amount}
+            {cart.length > 0 ? (
+                <div>
+                    <h2>Panier</h2>
+                    <ul>
+                        {cart.map(({ name, price, amount }, index) => (
+                            <div key={`${name}-${index}`}>
+                                {name} {price}€ x {amount}
+                            </div>
+                        ))}
+                    </ul>
+                    <h3>Total :{total}€</h3>
+                    <button onClick={() => updateCart([])}>Vider le panier</button>
                 </div>
-            ))}
-
-            <h3>Total : {total}€</h3>
-            <button onClick={() => updateCart([])}>Vider le panier</button>
+            ) : (
+                <div>Votre panier est vide</div>
+            )}
         </div>
     ) : (
         <div className='lmj-cart-closed'>
